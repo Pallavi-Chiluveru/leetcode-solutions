ad2583c4fc6f302583d494386a8810740bedf23d
Chiluveru Pallavi <pallavichiluveru537@gmail.com>
2026-07-24 23:05:28 +0530

    Time: 16 ms (88.41%), Space: 98.3 MB (5.53%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,4 +1,5 @@
 class Solution {
+
     List<List<Integer>> ans = new ArrayList<>();
 
     public List<List<Integer>> combine(int n, int k) {
@@ -7,16 +8,16 @@ class Solution {
     }
 
     public void backtrack(int start, int n, int k, List<Integer> list) {
-        
+
         if (list.size() == k) {
             ans.add(new ArrayList<>(list));
             return;
         }
 
         for (int i = start; i <= n; i++) {
-            list.add(i);                 
-            backtrack(i + 1, n, k, list); 
-            list.remove(list.size() - 1); 
+            list.add(i);
+            backtrack(i + 1, n, k, list);
+            list.remove(list.size() - 1);
         }
     }
 }
